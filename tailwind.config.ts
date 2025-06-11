b9fb67237e04e486cb541af11d05361487e73d48
Abhishek Mishra <abhimishraop08@gmail.com>
2025-06-11 23:24:22 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,53 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+      },
+      colors: {
+        // primary is now blue
+        primary: {
+          "100": "#DBEAFE",      // light blue-200
+          DEFAULT: "#3B82F6",   // blue-500
+        },
+        // secondary is now green
+        secondary: "#10B981",    // green-500
+        black: {
+          "100": "#333333",
+          "200": "#141413",
+          "300": "#7D8087",
+          DEFAULT: "#000000",
+        },
+        white: {
+          "100": "#F7F7F7",
+          DEFAULT: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px rgb(32, 138, 224)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
